a57915551a4989fe81ad7bfb2a80fdddc010af39
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2026-01-11 10:35:10 +0530

    final changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://your-frontend.vercel.app'
+    'https://code-sensei-jet.vercel.app/'
   ],
   credentials: true
 }));
