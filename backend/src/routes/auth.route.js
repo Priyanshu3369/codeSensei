e7da93e305a324ab656733ce7d277c35c3729d79
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2026-01-12 08:43:02 +0530

    fix auth

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,7 +3,7 @@ import { registerUser, loginUser } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/auth/register', registerUser);
-router.post('/auth/login', loginUser);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 export default router;
